1335bc841aa97bec6340d28eb268b53c0a23c433
glenMoutrie <glen.moutrie@gmail.com>
2016-02-14 22:30:08 +0000

    Newton Raphson function
    Basically a copy of the pseudo code on wikipedia, but I suspect this
    could be handy. Works well.

---
+++ R Files/NewtonRaphsonFunction.R
@@ -0,0 +1,38 @@
+# Newton Raphson
+
+newtonRaphson <- function(x0 = 1, f, fPrime, tolerance = 10^(-7), epsilon = 10^(-14),
+                          max.iterations = 20){
+  solution.found <- F
+  
+  for(i in 1:max.iterations) {
+    y <- f(x0)
+    y.prime <- fPrime(x0)
+    
+    # Ensure you don't divide by too small of a number
+    if(abs(y.prime) < epsilon) {
+      # Denominator is too small
+      break
+    }
+    
+    # Do newton's computation
+    x1 <- x0 - (y/y.prime)
+    
+    # If the result is within the desired tolerance
+    if (abs(x1 - x0) <= tolerance*abs(x1)){
+      solution.found <- T
+      break
+    }
+    
+    x0 <- x1
+  }
+  
+  if(solution.found) {
+    message("Succesful estimation")
+  } else {
+    message("Sucks to be you. No convergence.")
+  }
+  
+  return(x0)
+}
+
+newtonRaphson(f = function (x){x^2}, fPrime = function(x){2*x}, max.iterations = 100)
