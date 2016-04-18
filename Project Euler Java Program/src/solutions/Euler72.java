0e5e0d3fb2909630f23200647b6522126c4e9855
Glen Moutrie <glen.moutrie@gmail.com>
2016-04-18 18:19:51 +0100

    Solution for Euler 72
    Using the totient function

--- Project Euler Java Program/src/solutions/Euler72.java
+++ Project Euler Java Program/src/solutions/Euler72.java
@@ -44,27 +44,53 @@ public class Euler72 {
 	 * This is a better method using Euler's product formula for the totient function
 	 */
 	
-	public static int properFactors(int n) {
-		ArrayList<Integer> primes = new ArrayList<Integer>();
-		primes = basicTools.Factors.primeSieve(n);
+//	public static int properFactors(int n) {
+//		ArrayList<Integer> primes = new ArrayList<Integer>();
+//		primes = basicTools.Factors.primeSieve(n);
+//		
+//		// Incrementally find the next totient, and add this value to the total
+//		int totient;
+//		int totientMultiplier;
+//		int total;
+//		int index = 0;
+//		
+//		totientMultiplier = 1 - (1/primes.get(index));
+//		
+//		for(int i = 1; i <= n; i++) {
+//			
+//			// Update the totient
+//			while(primes.get(index) < i) {
+//				
+//			}
+//			
+//		}
+//		
+//		return(0);
+//	}
+	
+	public static long countFractions (int target) {
 		
-		// Incrementally find the next totient, and add this value to the total
-		int totient;
-		int totientMultiplier;
-		int total;
-		int index = 0;
-		
-		totientMultiplier = 1 - (1/primes.get(index));
-		
-		for(int i = 1; i <= n; i++) {
-			
-			// Update the totient
-			while(primes.get(index) < i) {
-				
-			}
-			
+		// Create phi vector and populate it with the range of numbers
+		int[] phi = new int[target +  2];
+		for (int i = 0; i < phi.length; i++) {
+			phi[i] = i;
 		}
 		
-		return(0);
+		long result = 0;
+		
+		for (int i = 2; i <= target; i ++) {
+			if (phi[i] == i) {
+				for (int j = i; j <= target; j += i) {
+					phi[j] = phi[j]/i*(i-1);
+				}
+			}
+			result += phi[i];
+		}
+		
+		return(result);
+	}
+	
+	public static void main(String args[]){
+		System.out.println(countFractions(1000000));
 	}
 }
