a359aed825139aed2884cb9ce9e6873ef05cba3c
glenMoutrie <glen.moutrie@gmail.com>
2016-06-23 14:13:34 +0100

    Update to algorithms

--- Project Euler Java Program/src/algorithms/Sort.java
+++ Project Euler Java Program/src/algorithms/Sort.java
@@ -43,6 +43,7 @@ public abstract class Sort {
 		return(unsorted);
 	}
 	
+<<<<<<< Updated upstream
 	private static Integer[] merge(Integer[] A, int p, int q, int r) {
 		
 		// Indexing for the two splits
@@ -56,11 +57,25 @@ public abstract class Sort {
 		
 		// Populate left
 		for(i = 0; i < nOne; i++) {
+=======
+	private static int[] merge(int[] A, int p, int q, int r) {
+		
+		// Indexing for the two splits
+		int nOne = r - q + 1;
+		int nTwo = q - p;
+		
+		// Initiate the two splits
+		int[] right = new int[nOne + 1];
+		int[] left = new int[nTwo + 1];
+		
+		// Populate left
+		for(int i = 0; i < nOne; i++) {
+>>>>>>> Stashed changes
 			left[i] = A[p + i];
 		}
-		
+			
 		// Populate right
-		for(j = 0; j < nTwo; j++) {
+		for(int j = 0; j < nTwo; j++) {
 			right[j] = A[q + j];
 		}
 		
@@ -70,6 +85,7 @@ public abstract class Sort {
 		left[nOne] = Integer.MAX_VALUE;
 		right[nTwo] = Integer.MAX_VALUE;
 		
+<<<<<<< Updated upstream
 		i = 0;
 		j = 0;
 		
@@ -81,10 +97,56 @@ public abstract class Sort {
 			} else {
 				A[k] = right[j];
 				j++;
+=======
+		int i = 0;
+		int j = 0;
+		
+		int[] result = new int[nOne + nTwo];
+		
+		// Merge left and right into a single array
+		for(int k = p; k <= r; k++) {
+			
+			System.out.println("Left: " + left[i] +", Right: " + right[j]);
+			if (left[i] <= right[j]) {
+				result[k] = left[i];
+				i++;
+			} else {
+				result[k] = right[j];
+				j++;
 			}
 		}
 		
-		return(A);
+		return(result);
+	}
+	
+	public static int[] mergeSort(int[]A, int p, int r) {
+		
+		//{5,2,4,7,1,3,2,6};
+		
+		if (p < r) {
+			
+			// Set up the mid point
+			int q = (int) Math.floor((p + r)/2);
+			System.out.println(q);
+			
+			// Copy the array to the new one (ugly as fuck, but right now I just want an implementation...)
+			System.arraycopy(mergeSort(A, p, q), 0, A, 0, q);
+			for(int i : A ){
+				System.out.println("First Merge Sort complete");
+				System.out.println(i);
+>>>>>>> Stashed changes
+			}
+			
+			System.arraycopy(mergeSort(A, q + 1, r), 0, A, q + 1, r);
+			
+			// Merge the two sorted results
+			A = merge(A, p, q, r);
+		} else {
+			System.out.println(A[p] + ", r = " + r);
+		}
+		
+		// Return a copy of the array
+		return(Arrays.copyOfRange(A,p,r + 1));
 	}
 	
 	public static Integer[] mergeSort(Integer[] A, int p, int r) {
@@ -135,6 +197,7 @@ public abstract class Sort {
 		System.out.println(sorted);
 		System.out.println(insertionSort(A, false));
 		
+<<<<<<< Updated upstream
 		Integer[] b = {2, 4, 5, 7, 1, 2, 3, 6};
 		
 //		System.out.println(merge(b,0,3,7));
@@ -143,6 +206,24 @@ public abstract class Sort {
 		for(Integer i : result) System.out.println(i);
 		
 		mergeSort(b, 0, 8);
+=======
+		int[] b = {2, 4, 5, 7, 1, 2, 3, 6};
+		
+		int[] result = merge(b,0,4,7);
+		
+		for(int i : result) System.out.println(i);
+		
+		int[] c = {5,2,4,7,1,3,2,6};
+		
+		System.out.println("Merge Sort check");
+		
+		int[] check = Arrays.copyOfRange(c, 0, 1);
+		for(int i : check) {System.out.println(i);}
+		
+		int[] sortResult = mergeSort(c, 0 , 7);
+		
+		for(int i : sortResult) {System.out.println(i);}
+>>>>>>> Stashed changes
 
 	}
 }
