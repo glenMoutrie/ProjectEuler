37f9a032488653a1bdbbfd1c1397c4a551fd53ed
Glen Moutrie <glenmoutrie@Glens-MacBook-Pro-5.local>
2017-01-23 08:24:17 +0000

    Updating PM project
    Working on the start of the project

--- PM Project/Scraper.py
+++ PM Project/Scraper.py
@@ -1,3 +1,14 @@
-import beautifulsoup;
+import numpy
+from bs4 import BeautifulSoup
+import pandas
+import sklearn
+import urllib
 
-print("winning")
+html_doc = urllib.urlopen("https://en.wikipedia.org/wiki/List_of_Prime_Ministers_of_the_United_Kingdom_by_tenure")
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+soup = soup.find_all("a");
+
+for x in soup:
+    print x
