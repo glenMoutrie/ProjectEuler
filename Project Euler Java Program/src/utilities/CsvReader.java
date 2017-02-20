d39c97f3ed57c4a5f5271a8badc91a35e2f48703
Glen Moutrie <glen.moutrie@gmail.com>
2017-02-20 12:54:35 +0000

    I think I have broken everything
    Classpath issues left right and centre, I think I caused this by
    running dual instances of eclipse….

--- Project Euler Java Program/src/utilities/CsvReader.java
+++ Project Euler Java Program/src/utilities/CsvReader.java
@@ -1,18 +1,56 @@
 package utilities;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class CsvReader {
-	String fileName;
+	
 	boolean hasColNames;
 	boolean hasRowNames;
+	File csvFile;
+	FileInputStream stream;
 	
 	// Constructors
-	public CsvReader(String fileName, boolean hasColNames, boolean hasRowNames) {
+	public CsvReader(String path, boolean hasColNames, boolean hasRowNames) throws FileNotFoundException, IOException {
 		
+		setUpConnection(path);
+		
+		readCSV();
 		
 	}
+	
 	public CsvReader(String fileName){
 		hasColNames = false;
 		hasRowNames = false;
+	}
+	
+	private void setUpConnection(String path) throws FileNotFoundException {
+		
+		this.csvFile = new File(path);
+		this.stream = new FileInputStream(this.csvFile);
+		
+	}
+	
+	private void readCSV() throws IOException {
+		System.out.println(this.stream.read());
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Yay...");
+		
+//		try {
+//			
+//			CsvReader test = new CsvReader("~/Documents/Data/Boston.csv", true, false);
+//			
+//		} catch (FileNotFoundException PathError)  {
+//			
+//		} catch (IOException IOError)  {
+//			
+//		}
+		
 	}
 
 }
